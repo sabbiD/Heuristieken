434479b94e38e8198e3c4b46b2d4cbbf6958e126
SammyH1994 <36191292+SammyH1994@users.noreply.github.com>
2018-05-03 10:28:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# bla
+# voeg de huidige structuur toe aan path
+import os, sys
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(directory, "Code"))
+sys.path.append(os.path.join(directory, "Code", "classes"))
+sys.path.append(os.path.join(directory, "code", "algoritmes"))
+
+# importeer de gebruikte structuur
+from datastructuur import DataStructuur
+from bruteforce import randomize
+from hillclimber import hill_climber
+from breadthfirst import breadth_first
+from genetic import genetic
+from score import score
