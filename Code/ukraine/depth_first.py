507a2b9cf3f2c7c5c0a50a54776b715aa17a3fbe
Krosenblad <kajsa.rosenblad@student.uva.nl>
2018-05-08 14:54:44 +0200

    start of depth first algorithm

--- Code/ukraine/depth_first.py
+++ Code/ukraine/depth_first.py
@@ -1,6 +1,41 @@
 from ukraine import country
-from data_structure import data_structure
+from kgbUA import final_regions
 
-regions = country()
-data_structure = data_structure(regions)
+# http://www.koderdojo.com/blog/depth-first-search-in-python-recursive-and-non-recursive-programming
+data = final_regions
+
+def dfs(graph, start):
+	stack, path = [start], []
+	
+	
+	while stack:
+		vertex = stack.pop()
+		
+		if vertex in path:
+			continue
+		else:
+			path.append(vertex)	
+
+		for neighbour in graph[vertex]:
+			print(graph[vertex])
+			
+			if vertex.radio == neighbour.radio:
+				#print(vertex.radio)
+				vertex.radio += 1
+
+			#print(neighbour)
+			stack.append(neighbour)
+	#print(path)
+	return path
+
+start = list(final_regions.keys())[0]
+
+
+d = dfs(data, start)
+print(d)
+
+
+
+
+
 
