b37892b5c02f271b283fc9d7e1510d112d5b8a54
Krosenblad <kajsa.rosenblad@student.uva.nl>
2018-05-22 14:54:20 +0200

    csv writer

--- Code/experimentation/csv_writer.py
+++ Code/experimentation/csv_writer.py
@@ -1,11 +1,12 @@
 import csv
-from distribution import compare
-from score import score
-from costs import costs
-from costs import distribution
+# from distribution import compare
+# from score import score
+# from costs import costs
+# from costs import distribution
 
 
 def write_csv(compare, name):
+	
 	name = name + '.csv'
 	score = compare[0]
 	cost = compare[1]
@@ -16,34 +17,30 @@ def write_csv(compare, name):
 	fail = compare[6]
 
 	lowest= []
+	cheapest = []
 
 	for i in cost:
 		lowest_key = min(i, key=i.get)
 		value = i.get(lowest_key)
-		lowest.append([lowest_key, value])
+		lowest.append(value)
+		cheapest.append(lowest_key)
 	
-
-	lowest_v = 1000000
-
-	for n in lowest:
-		
-		if (lowest_v > n[1]):
-			lowest_v = n[1]
+	lowest_v = min(lowest)
 
 	with open('../Heuristieken/Results/{}'.format(name), 'w') as csvfile:
 		fieldnames = ['Try No', 'Score', 'Even Distribution Score', 'Cheapest Station', 'Costs', 'Amount of Stations',
-		 'Lowest price', 'Time', 'Fails', 'How many Stations']
+		 'Lowest price', 'Average Time', 'Fails', 'How many Stations']
 		writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
 
 		writer.writeheader()
 		for i in range(len(score)):
 			writer.writerow({'Try No': i,'Score': score[i], 
 				'Even Distribution Score': even[i], 
-				'Cheapest Station': lowest[i][0], 'Costs':lowest[i][1],
+				'Cheapest Station': cheapest[i], 'Costs':lowest[i],
 				 'Amount of Stations':amount[i]})
 			
 		
-		writer.writerow({'Lowest price': lowest_v, 'Time': time, 'Fails': fail, 'How many Stations': used})
+		writer.writerow({'Lowest price': lowest_v, 'Average Time': time, 'Fails': fail, 'How many Stations': used})
 
 
 
